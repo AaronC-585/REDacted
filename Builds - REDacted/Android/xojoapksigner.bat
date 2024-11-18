2d0d8d8d39f3ca4da82c9df85aa9b38b4e391144
Aaron C <aarondamiencalder@gmail.com>
2024-11-18 02:16:19 -0500

    login prompt done with status icons

--- Builds - REDacted/Android/xojoapksigner.bat
+++
@@ -1,2 +0,0 @@
-set JAVA_HOME=C:\Program Files\Android\Android Studio\jbr\
-"C:\Users\aaron\AppData\Local\Android\Sdk\BUILD-~1\350~1.0\APKSIG~1.BAT" sign --ks "D:\Android_dev_key.jks" --ks-key-alias "kirc" --ks-pass pass:"Damien84~" --key-pass pass:"Damien84~" "D:\REDacted\Builds - REDacted\Android\REDacted.apk"
